79651792f56b132c9192c6e153b78c75523ec87f
Katie Broida <katie.broida@upstatement.com>
2017-02-10 15:26:26 -0500

    Selected text prints to console

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 browser.contextMenus.create({
   id: "Mologi",
   title: "Search with Mologi",
-  contexts: ["all"]
+  contexts: ["selection"]
+});
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  console.log(info.selectionText);
 });
